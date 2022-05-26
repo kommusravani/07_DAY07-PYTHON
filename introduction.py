2ba6bbf3ed659417d038b5cd53619ecd236dc974
kommusravani <104352484+kommusravani@users.noreply.github.com>
2022-05-26 12:04:31 +0530

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,23 @@
+""" name:bhargavi
+ fle decs : python 
+ date:05-05-2022
+ """
+print('hello python')
+print("python world")
+print('''hello world''')
+print("""hello world""")
+
+print('hell\'o python')
+print("hell\'o python")
+print('''hell\'o python''')
+print("""hell\'o python""")
+
+print('Hell"o python')
+print("Hell\"o python")
+print('''Hell"o python''')
+print("""Hell"o python""")
+
+print('print(hello python)')
+print('''print(hello python)''')
+print("print(hello python)")
+print("""print(hello python)""")
---
+++ taking_input.py
@@ -0,0 +1,17 @@
+""" NAME: BHARGAVI
+DATE:18-MAY-2022
+FILE DECS: TAKING_INPUT
+"""
+
+# print('python'+'by','GUIDO VAN', end=".",sep="-") #pythonby-GUIDO VAN.
+
+# print('hi','bye',sep=' ')
+# print('hello') #hello
+# print('hi') #hi
+# print('how are you') #how are you
+# print('py') #py
+# print('thon') #thon
+
+a=input()
+b=input()
+print(a*b)
